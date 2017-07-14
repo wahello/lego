1b0d298466dd351ac46fd999eaee6299790c215e
Hans-Wilhelm Warlo <hw@warlo.no>
2017-07-14 23:38:43 +0200

    Survey serializer things

--- lego/apps/surveys/serializers.py
+++ lego/apps/surveys/serializers.py
@@ -7,26 +7,19 @@ from lego.apps.users.serializers.users import PublicUserSerializer
 from lego.utils.serializers import BasisModelSerializer
 
 
-class QuestionSerializer(BasisModelSerializer):
-    question_type = serializers.ChoiceField(choices=constants.ALTERNATIVE_TYPES)
-
+class QuestionReadSerializer(BasisModelSerializer):
     class Meta:
         model = Question
         fields = ('id', 'question_type', 'question_text',  'mandatory')
 
-    def create(self, validated_data):
-        survey = Survey.objects.get(pk=self.context['view'].kwargs['survey_pk'])
-        question = Question.objects.create(survey=survey, **validated_data)
-        question.relative_index = question.survey.questions.count() + 1
-        question.save()
-        return question
 
-
-class AlternativeSerializer(BasisModelSerializer):
+class AlternativeCreateAndUpdateSerializer(BasisModelSerializer):
+    alternative_type = serializers.ChoiceField(choices=constants.ALTERNATIVE_TYPES)
 
     class Meta:
         model = Alternative
         fields = ('id', 'alternative_text', 'alternative_type')
+        extra_kwargs = {'id': {'read_only': False, 'required': False}}
 
     def create(self, validated_data):
         question = Question.objects.get(pk=self.context['view'].kwargs['question_pk'])
@@ -42,6 +35,22 @@ class AlternativeSerializer(BasisModelSerializer):
         elif question.question_type == 3 and value == 3:
             return value
         raise serializers.ValidationError('This alternative type does not match the question type')
+
+
+class QuestionCreateAndUpdateSerializer(BasisModelSerializer):
+    alternatives = AlternativeCreateAndUpdateSerializer(many=True)
+    question_type = serializers.ChoiceField(choices=constants.ALTERNATIVE_TYPES)
+
+    class Meta:
+        model = Question
+        fields = ('id', 'question_type', 'question_text',  'mandatory', 'alternatives')
+        extra_kwargs = {'id': {'read_only': False, 'required': False}}
+
+    def create(self, validated_data):
+        survey = Survey.objects.get(pk=self.context['view'].kwargs['survey_pk'])
+        relative_index = survey.question.count() + 1
+        question = Question.objects.create(survey=survey, relative_index=relative_index, **validated_data)
+        return question
 
 
 class AnswerSerializer(BasisModelSerializer):
@@ -69,7 +78,7 @@ class SubmissionReadSerializer(BasisModelSerializer):
 
 class SurveyReadDetailedSerializer(BasisModelSerializer):
     submissions = SubmissionReadSerializer(many=True)
-    questions = QuestionSerializer(many=True)
+    questions = QuestionReadSerializer(many=True)
     event = EventReadSerializer()
 
     class Meta:
@@ -79,28 +88,28 @@ class SurveyReadDetailedSerializer(BasisModelSerializer):
 
 
 class SurveyCreateAndUpdateSerializer(BasisModelSerializer):
-    clone_id = serializers.IntegerField()
+    clone_id = serializers.IntegerField(required=False)
+    questions = QuestionCreateAndUpdateSerializer(many=True)
 
     class Meta:
         model = Survey
         fields = ('id', 'title', 'active_from', 'clone_id', 'event', 'questions')
 
     def create(self, validated_data):
-        clone_id = validated_data.pop('clone_id', None)
-        event = validated_data.pop('event', None)
-        questions = validated_data.pop('questions', None)
+        clone_id = validated_data.get('clone_id', None)
+        event = validated_data.get('event', None)
+        questions = validated_data.pop('questions', [])
         if clone_id:
             template = Template.objects.get(pk=clone_id)
             survey = template.generate_survey(event, validated_data)
         else:
-            survey = Survey.objects.create(**validated_data)
+            survey = super().create(validated_data)
 
-        if questions:
-            for question in questions:
-                alternatives = questions.pop('alternatives', None)
-                Question.objects.create(**question)
-                for alternative in alternatives:
-                    Alternative.objects.create(**alternative)
+        for question in questions:
+            alternatives = question.pop('alternatives', [])
+            Question.objects.create(survey=survey, **question)
+            for alternative in alternatives:
+                Alternative.objects.create(question=question, **alternative)
         return survey
 
 
